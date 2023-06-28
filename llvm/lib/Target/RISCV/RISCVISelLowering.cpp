495938bdebb1039a30dbc865d8fb7202f96196bf
kinodjnz <kinodjnz@gmail.com>
2023-06-29 02:50:07 +0900

    cmov insn

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -411,8 +411,12 @@ RISCVTargetLowering::RISCVTargetLowering(const TargetMachine &TM,
 
   if (Subtarget.useCCMovInsn())
     setOperationAction(ISD::SELECT, XLenVT, Legal);
-  else if (!Subtarget.hasVendorXTHeadCondMov())
+  else if (!Subtarget.hasVendorXTHeadCondMov()) {
     setOperationAction(ISD::SELECT, XLenVT, Custom);
+  }
+
+  if (Subtarget.hasVendorXCramp())
+    setOperationAction(ISD::SELECT, XLenVT, Legal);
 
   static const unsigned FPLegalNodeTypes[] = {
       ISD::FMINNUM,       ISD::FMAXNUM,        ISD::FMINIMUMNUM,
